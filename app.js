b0ddcdff378a6a6707f235efd915e5b9d3801364
Caleb Phillips <csphillips5@gmail.com>
2022-08-06 19:10:48 -0400

    initialize server entry point

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+require('dotenv').config({ path: './config/.env' });
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`)
+});
