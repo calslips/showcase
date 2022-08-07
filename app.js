17b6d2f88cd97c69ba2dc5601499818672f19bc8
Caleb Phillips <csphillips5@gmail.com>
2022-08-07 17:36:39 -0400

    initialize express-handlebars

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan')
 require('dotenv').config({ path: './config/.env' });
 const connectDB = require('./config/db');
+const { engine } = require('express-handlebars');
 
 connectDB();
 const app = express();
@@ -9,6 +10,10 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
 const PORT = process.env.PORT || 3000;
 
