a6c8360afab3fcdbc1a659f352eef4b5cc663611
Caleb Phillips <csphillips5@gmail.com>
2022-08-10 23:06:21 -0400

    define route handler to GET /dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuth, ensureUser } = require('../middleware/auth');
+const Entry = require('../models/Entry');
 
 // @desc    Login/Landing page
 // @route   GET /
@@ -12,10 +13,17 @@ router.get('/', ensureUser, (req, res) => {
 
 // @desc    Dashboard page
 // @route   GET /dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
-  res.render('dashboard', {
-    name: req.user.firstName,
-  });
+router.get('/dashboard', ensureAuth, async (req, res) => {
+  try {
+    const entries = await Entry.find({ user: req.user._id }).lean();
+    res.render('dashboard', {
+      name: req.user.firstName,
+      entries,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render('error/500');
+  }
 });
 
 module.exports = router;
